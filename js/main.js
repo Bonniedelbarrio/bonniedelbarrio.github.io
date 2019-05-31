681e06c1e67923a822c853e17e2ea4a82b9943d9
Bonniedelbarrio <36390745+Bonniedelbarrio@users.noreply.github.com>
2019-05-31 04:57:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ if ( Detector.webgl ) {
 function basicSetup(){
 	// INIT SCENE
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0xad1d6f );
+	scene.background = new THREE.Color( 0x3a3a3a );
 	scene.fog = new THREE.Fog( 0x050505, 1, 15 );
 
 	// INIT CAMERA
