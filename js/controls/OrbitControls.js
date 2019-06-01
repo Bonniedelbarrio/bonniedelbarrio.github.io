5547c8660b0417d2061de08ab446de5e90788131
Bonniedelbarrio <36390745+Bonniedelbarrio@users.noreply.github.com>
2019-06-01 01:13:07 -0500

    Update OrbitControls.js

--- js/controls/OrbitControls.js
+++ js/controls/OrbitControls.js
@@ -45,8 +45,8 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 	// Set to true to enable damping (inertia)
 	// If damping is enabled, you must call controls.update() in your animation loop
-	this.enableDamping = false;
-	this.dampingFactor = 0.25;
+	this.enableDamping = true;
+	this.dampingFactor = 0.50;
 
 	// This option actually enables dollying in and out; left as "zoom" for backwards compatibility.
 	// Set to false to disable zooming
